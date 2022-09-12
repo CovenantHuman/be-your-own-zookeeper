2294b6c5f99d4389b98ef0c45f8795b93761ea3b
Michaela Healton <mshealton@gmail.com>
2022-09-12 14:43:13 -0700

    Added form to homepage

--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from flask import(Flask, render_template, request, flash, session, redirect)
 from jinja2 import StrictUndefined
+import os
 
 app = Flask(__name__)
 app.secret_key = "dev"
 app.jinja_env.undefined = StrictUndefined
+OPEN_WEATHER_KEY = os.environ["OPEN_WEATHER_KEY"]
 
 @app.route("/")
 def homepage():
