30ab1762e4e51a66ab8ceb7d17ef861f8b4e1757
Michaela Healton <mshealton@gmail.com>
2022-09-12 12:53:29 -0700

    Initial commit

---
+++ server.py
@@ -0,0 +1,16 @@
+"""Server for self care app."""
+
+from flask import(Flask, render_template, request, flash, session, redirect)
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
+
+@app.route("/")
+def homepage():
+    """Show homepage"""
+    return render_template("homepage.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
