9ca8eac14e870462f7836f55d93c569718ba11bc
Michaela Healton <mshealton@gmail.com>
2022-09-14 14:34:17 -0700

    Added login and logout functionality

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import requests
 import os
 import crud
 from model import connect_to_db, db
+from passlib.hash import pbkdf2_sha256
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -13,20 +14,18 @@ app.jinja_env.undefined = StrictUndefined
 OPEN_WEATHER_KEY = os.environ["OPEN_WEATHER_KEY"]
 
 @app.route("/")
-def homepage():
+def show_homepage():
     """Show homepage"""
     return render_template("homepage.html")
 
 @app.route("/new-account")
-def new_account():
+def show_create_account_page():
     """Show the create an account page."""
     return render_template("create_account.html")
 
 @app.route("/create-account", methods=["POST"])
-def create_account():
-    """Creates a new user account."""
-    # TODO: Once I have users in the database add a test here to make sure that 
-    # email is not already in use before creating new account.
+def process_create_account():
+    """Create a new user account"""
     name = request.form.get("name")
     email = request.form.get("email")
     password = request.form.get("password")
@@ -40,9 +39,33 @@ def create_account():
         flash("Thanks for creating an account! Please login.")
         return redirect("/")
 
+@app.route("/login")
+def show_login_page():
+    """Show the login page"""
+    return render_template("login.html")
+
+@app.route("/login-user", methods=["POST"])
+def process_login():
+    """Log in user if password is correct."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    user = crud.get_user_by_email(email)
+    if pbkdf2_sha256.verify(password, user.password):
+        session['user_id'] = user.user_id
+        flash('Logged in!')
+    else:
+        flash('Not logged in!')
+    return redirect("/")
+
+@app.route("/logout")
+def process_logout():
+    """Log out user"""
+    del session['user_id']
+    flash("You have been logged out!")
+    return redirect("/")
 
 @app.route("/zip-form")
-def get_weather():
+def process_get_weather():
     """Take in the zipcode and gets the weather"""
     zipcode = request.args.get("zipcode")
     zip_with_country_code = zipcode + ",us"
