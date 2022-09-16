1910c2849dfd3930ae7d5e0e27edcaf0745594cb
Michaela Healton <mshealton@gmail.com>
2022-09-15 22:47:16 -0700

    Made weather report show on page load for logged in users

--- static/js/user_homepage.js
+++ static/js/user_homepage.js
@@ -1,6 +1,10 @@
 'use strict';
 import getWeather from "./weather.js"; 
 
+document.addEventListener('DOMContentLoaded', () => {
 
+  const zipcode = document.querySelector('#zipcode-field').value;
+  getWeather(zipcode)
+});
 
 
