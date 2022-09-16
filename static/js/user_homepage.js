3f74dedf8e46ef19d4a5ffbdb645cfe5768ab06f
Michaela Healton <mshealton@gmail.com>
2022-09-15 23:05:39 -0700

    Added button functionality to user_homepage

--- static/js/user_homepage.js
+++ static/js/user_homepage.js
@@ -8,3 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+const weatherForm = document.querySelector('.get-weather');
+
+weatherForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const zipcode = document.querySelector('#zipcode-field').value;
+  getWeather(zipcode)
+});
