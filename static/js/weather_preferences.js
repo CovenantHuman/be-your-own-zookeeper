18e49bb5ab247eb547949d507a4d54238b7cacd8
Michaela Healton <mshealton@gmail.com>
2022-09-20 15:52:42 -0700

    Set boundaries for all numerical inputs on the front end

--- static/js/weather_preferences.js
+++ static/js/weather_preferences.js
@@ -22,3 +22,26 @@ fahrenheitSelector.addEventListener('change', () => {
         minTempBox.setAttribute("min", "0");
     }
 });
+
+maxTempBox.addEventListener("change", () => {
+    const tempMax = maxTempBox.value;
+    minTempBox.setAttribute("max", String(tempMax));
+});
+
+minTempBox.addEventListener("change", () => {
+    const tempMin = minTempBox.value;
+    maxTempBox.setAttribute("min", String(tempMin));
+});
+
+const maxCloudBox = document.querySelector("#max_clouds");
+const minCloudBox = document.querySelector("#min_clouds");
+
+maxCloudBox.addEventListener("change", () => {
+    const cloudMax = maxCloudBox.value;
+    minCloudBox.setAttribute("max", String(cloudMax));
+});
+
+minCloudBox.addEventListener("change", () => {
+    const cloudMin = minCloudBox.value;
+    maxCloudBox.setAttribute("min", String(cloudMin));
+});
