0e9a1411755e9eaa9d88e0e56e68a99346755e95
Michaela Healton <mshealton@gmail.com>
2022-09-22 15:43:10 -0700

    Set up button disabling for when I get around to adding CSS.

--- static/js/checklist.js
+++ static/js/checklist.js
@@ -10,7 +10,9 @@ const checklistNo = document.querySelector('.show-advice');
 
 checklistNo.addEventListener('click', (evt) => {
   evt.preventDefault();
-
+  const checklistYes = document.querySelector('.yes-option')
   const advice = document.querySelector('.advice');
   advice.style.display = "block";
+  checklistYes.setAttribute("class", "disabled");
+  checklistNo.setAttribute("class", "disabled");
 });
