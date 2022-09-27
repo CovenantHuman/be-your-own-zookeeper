1c4d5bf581dc22f3a8acb1d57607fcf72dac5759
Michaela Healton <mshealton@gmail.com>
2022-09-27 16:09:26 -0700

    Create a file that sends messages using twilio if the current time is the time at which an event is meant to happen.

--- crud.py
+++ crud.py
@@ -1,7 +1,5 @@
 """Crud operations"""
 
-from ast import Delete
-from venv import create
 from model import db, User, ChecklistItem, Event, Activity, connect_to_db
 import uuid
 from passlib.hash import pbkdf2_sha256
@@ -122,7 +120,8 @@ def create_default_activities(user):
     activities.append(create_activity(user, "Push ups"))
     return activities
 
-def get_activity_by_name(user, name):
+def get_activity_by_user_and_name(user, name):
+    """Get activity for a given user by name"""
     for activity in user.activities:
         if activity.name == name:
             return activity
@@ -138,6 +137,10 @@ def create_event(user, event_type, time, description, reminder):
                 description=description,
                 reminder=reminder)
     return event
+
+def get_events_by_time(time):
+    return Event.query.filter(Event.time == time).all()
+
 
 if __name__ == "__main__":
     from server import app
---
+++ send_messages.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+import crud
+from model import connect_to_db
+import os
+from twilio.rest import Client
+
+def send_message(message):
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+                                    body=f"{message}",
+                                    from_='+17087873292',
+                                    to='+17639130524'
+                                    )
+
+    print(message.sid)
+
+def message_sender():
+    now = datetime.now()
+    now = datetime.strftime(now, "%I:%M %p")
+    now = datetime.strptime(now, "%I:%M %p")
+
+    events = crud.get_events_by_time(now)
+    for event in events:
+        send_message(event.description)
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+
+    message_sender()
+
+
+
+
