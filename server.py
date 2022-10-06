9ffcee5b88d1258f920090d6c1bacb61e4e30537
Michaela Healton <mshealton@gmail.com>
2022-10-06 17:50:18 -0500

    Made it so non-logged-in users are redirected to homepage from any page they don't have permission to see.

--- server.py
+++ server.py
@@ -68,26 +68,32 @@ def process_login():
 @app.route("/user-homepage")
 def show_user_homepage():
     """Show user logged in homepage"""
-    user = crud.get_user_by_email(session["user_email"])
-    return render_template("/user_homepage.html", name=user.name, zipcode=user.zipcode)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session["user_email"])
+        return render_template("/user_homepage.html", name=user.name, zipcode=user.zipcode)
+    else:
+        return redirect("/")
 
 @app.route("/weather-preferences")
 def show_weather_preferences():
     """Show weather preferences page"""
-    user = crud.get_user_by_email(session["user_email"])
-    return render_template("weather_preferences.html", 
-                            temp_unit=user.is_fahrenheit, 
-                            max_temp=user.max_temp, 
-                            min_temp=user.min_temp,
-                            max_hum=user.max_hum,
-                            wind_unit=user.is_imperial,
-                            max_wind_speed=user.max_wind_speed,
-                            max_clouds=user.max_clouds,
-                            min_clouds=user.min_clouds,
-                            rain=user.rain,
-                            snow=user.snow,
-                            day=user.daylight,
-                            night=user.night)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session["user_email"])
+        return render_template("weather_preferences.html", 
+                                temp_unit=user.is_fahrenheit, 
+                                max_temp=user.max_temp, 
+                                min_temp=user.min_temp,
+                                max_hum=user.max_hum,
+                                wind_unit=user.is_imperial,
+                                max_wind_speed=user.max_wind_speed,
+                                max_clouds=user.max_clouds,
+                                min_clouds=user.min_clouds,
+                                rain=user.rain,
+                                snow=user.snow,
+                                day=user.daylight,
+                                night=user.night)
+    else:
+        return redirect("/")
 
 @app.route("/update-weather-preferences", methods=["POST"])
 def set_weather_preferences():
@@ -149,12 +155,15 @@ def set_weather_preferences():
 @app.route("/alternate-activities")
 def show_alternate_activities():
     """Show alternate activities editing page"""
-    user = crud.get_user_by_email(session['user_email'])
-    names = []
-    for activity in user.activities:
-        name_tuple = (activity.name, activity.activity_id)
-        names.append(name_tuple)
-    return render_template("alternate_activities.html", names=names)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        names = []
+        for activity in user.activities:
+            name_tuple = (activity.name, activity.activity_id)
+            names.append(name_tuple)
+        return render_template("alternate_activities.html", names=names)
+    else:
+        return redirect("/")
 
 @app.route("/remove-alt-act/<id>")
 def remove_alternate_activity(id):
@@ -179,19 +188,25 @@ def add_alternate_activity():
 @app.route("/schedule")
 def show_schedule():
     """Show schedule page"""
-    user = crud.get_user_by_email(session['user_email'])
-    user_events = user.events
-    sorted_events = sorted(user_events, key= lambda event: event.time)
-    events = []
-    for sorted_event in sorted_events:
-        user_time = sorted_event.time.strftime("%I:%M %p")
-        events.append({"time": user_time, "description": sorted_event.description, "id":sorted_event.event_id})
-    return render_template("schedule.html", name=user.name, events=events)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        user_events = user.events
+        sorted_events = sorted(user_events, key= lambda event: event.time)
+        events = []
+        for sorted_event in sorted_events:
+            user_time = sorted_event.time.strftime("%I:%M %p")
+            events.append({"time": user_time, "description": sorted_event.description, "id":sorted_event.event_id})
+        return render_template("schedule.html", name=user.name, events=events)
+    else:
+        return redirect("/")
 
 @app.route("/new-event")
 def show_add_event_page():
     """Show add event page"""
-    return render_template("add_event.html")
+    if session.get("user_email"):
+        return render_template("add_event.html")
+    else:
+        return redirect("/")
 
 @app.route("/add-event", methods=["POST"])
 def add_event():
@@ -216,13 +231,16 @@ def add_event():
 @app.route("/event-edit/<id>")
 def show_edit_event(id):
     """Display the edit event page"""
-    event = crud.get_event_by_id(id)
-    return render_template("edit_event.html", 
-                            id=id, 
-                            event_type=event.event_type, 
-                            time=datetime.datetime.strftime(event.time, "%H:%M"), 
-                            description=event.description, 
-                            reminder=event.reminder)
+    if session.get("user_email"):
+        event = crud.get_event_by_id(id)
+        return render_template("edit_event.html", 
+                                id=id, 
+                                event_type=event.event_type, 
+                                time=datetime.datetime.strftime(event.time, "%H:%M"), 
+                                description=event.description, 
+                                reminder=event.reminder)
+    else:
+        return redirect("/")
 
 @app.route("/edit-event/<id>", methods=["POST"])
 def edit_event(id):
@@ -255,46 +273,58 @@ def remove_event(id):
 @app.route("/checklist-start")
 def show_checklist_landing_page():
     """Show checklist landing page"""
-    user = crud.get_user_by_email(session['user_email'])
-    return render_template("checklist_start.html", name=user.name)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        return render_template("checklist_start.html", name=user.name)
+    else:
+        return redirect("/")
 
 @app.route("/checklist-item/<order>")
 def show_checklist_item(order):
     """Show a checklist item"""
-    user = crud.get_user_by_email(session['user_email'])
-    checklist_list = list(user.checklist_items)
-    checklist_item_count = len(checklist_list)
-    if int(order) >= checklist_item_count+1:
-        return render_template("checklist_end.html",
-                                name=user.name,
-                                total=checklist_item_count+1)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        checklist_list = list(user.checklist_items)
+        checklist_item_count = len(checklist_list)
+        if int(order) >= checklist_item_count+1:
+            return render_template("checklist_end.html",
+                                    name=user.name,
+                                    total=checklist_item_count+1)
+        else:
+            for item in checklist_list:
+                if item.order == int(order):
+                    return render_template("checklist.html", 
+                                            name=user.name,
+                                            question=item.question, 
+                                            advice=item.advice,
+                                            order=item.order,
+                                            total=checklist_item_count+1) 
     else:
-        for item in checklist_list:
-            if item.order == int(order):
-                return render_template("checklist.html", 
-                                        name=user.name,
-                                        question=item.question, 
-                                        advice=item.advice,
-                                        order=item.order,
-                                        total=checklist_item_count+1) 
+        return redirect("/")
 
 @app.route("/edit-checklist")
 def show_checklist_edit_page():
     """Show checklist editing page"""
-    user = crud.get_user_by_email(session['user_email'])
-    items = []
-    for item in crud.get_user_checklist_in_order(user):
-        item_info = [item.question, item.item_id, item.order]
-        items.append(item_info)
-    return render_template("checklist_edit.html", items=items)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        items = []
+        for item in crud.get_user_checklist_in_order(user):
+            item_info = [item.question, item.item_id, item.order]
+            items.append(item_info)
+        return render_template("checklist_edit.html", items=items)
+    else:
+        return redirect("/")
 
 @app.route("/new-checklist-item")
 def show_new_checklist_item_page():
     """Show page for adding a new item to the checklist."""
-    user = crud.get_user_by_email(session['user_email'])
-    items = user.checklist_items
-    count = len(items) + 1 
-    return render_template("add_checklist_item.html", count=count)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        items = user.checklist_items
+        count = len(items) + 1 
+        return render_template("add_checklist_item.html", count=count)
+    else:
+        return redirect("/")
 
 @app.route("/add-checklist-item", methods=["POST"])
 def add_checklist_item():
@@ -312,16 +342,19 @@ def add_checklist_item():
 @app.route("/edit-individual-checklist-item/<id>")
 def show_edit_checklist_item_page(id):
     """Display edit checklist item page"""
-    item = crud.get_checklist_item_by_id(id)
-    user = crud.get_user_by_email(session["user_email"])
-    items = user.checklist_items
-    count = len(items) 
-    return render_template("edit_checklist_item.html", 
-                            id=item.item_id, 
-                            question=item.question,
-                            advice=item.advice,
-                            position=item.order,
-                            count=count)
+    if session.get("user_email"):
+        item = crud.get_checklist_item_by_id(id)
+        user = crud.get_user_by_email(session["user_email"])
+        items = user.checklist_items
+        count = len(items) 
+        return render_template("edit_checklist_item.html", 
+                                id=item.item_id, 
+                                question=item.question,
+                                advice=item.advice,
+                                position=item.order,
+                                count=count)
+    else:
+        return redirect("/")
 
 @app.route("/edit-checklist-item/<id>", methods=["POST"])
 def edit_chcklist_item(id):
@@ -350,12 +383,15 @@ def remove_checklist_item(id):
 @app.route("/account-preferences")
 def show_account_preferences():
     """Show account preferences page"""
-    user = crud.get_user_by_email(session['user_email'])
-    return render_template("account_preferences.html",
-                            name=user.name,
-                            email=user.email,
-                            zipcode=user.zipcode,
-                            phone=user.phone)
+    if session.get("user_email"):
+        user = crud.get_user_by_email(session['user_email'])
+        return render_template("account_preferences.html",
+                                name=user.name,
+                                email=user.email,
+                                zipcode=user.zipcode,
+                                phone=user.phone)
+    else:
+        return redirect("/")
 
 @app.route("/update-account-preferences", methods=["POST"])
 def update_account_preferences():
